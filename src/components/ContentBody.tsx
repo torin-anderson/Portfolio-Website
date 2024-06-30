08cf02c79e33fb78c17f5bc6cd8810bdf0e24116
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-06-30 17:54:13 -0400

    Update ContentBody.tsx

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,35 +1,35 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
-import { Content } from "@prismicio/client";
 
+import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 
 
 export default function ContentBody({
   page,
 }: {
-  page: Content.ProsePostDocument | Content.ProjectDocument;
+  page: Content.BlogPostDocument | Content.ProjectDocument;
 }) {
 
   return (
     <Bounded as="article">
-      <div className="rounded-2xl border-2 border-violet-500 bg-violet-800 px-4 py-10 md:px-8 md:py-20">
+    <div className="rounded-2xl border-2 border-indigo-900 bg-indigo-950 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
-        <div className="flex gap-4 text-yellow-400">
-          {page.tags.map((tag, index) => (
-            <span key={index} className="text-xl font-bold">
-              {tag}
-            </span>
-          ))}
+        <div className="flex gap-4 text-yellow-400 text-xl font-bold">
+            {page.tags.map((tag, index)=>(
+                <span key={tag}>{tag}</span>
+            ))}
         </div>
-        <p className="mt-8 border-b border-violet-600 text-xl font-medium text-violet-300">
-          {page.data.date}
+        <p className="mt-8 border-b border-indigo-600 text-xl font-medium text-indigo-300">
+            {page.data.date}
         </p>
         <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
-          <SliceZone slices={page.data.slices} components={components} />
+            <SliceZone slices={page.data.slices} components={components} />
         </div>
-      </div>
+    </div>
     </Bounded>
-  );
+    );
 }
