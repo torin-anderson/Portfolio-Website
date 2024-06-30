a246d123bdda90fded1c5094f48ba45148af965a
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-06-30 18:09:41 -0400

    Update ContentBody.tsx

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
-
+import { Content } from "@prismicio/client";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import Bounded from "@/components/Bounded";
